ad566b4d83674fb2a4374a07fe47a6a98aa15b19
Katarzyna Juchniewicz <juchkasia@gmail.com>
2022-12-29 17:28:09 +0100

    feat: add folder assets types.ts

--- develop/src/index.ts
+++ develop/src/index.ts
@@ -4,3 +4,11 @@ import "../index";
 import "../style.css";
 import "../style.scss";
 import "../normalize.css";
+
+// function addNumbers(x: number, y: number) {
+//     return x + y;
+//   }
+//   console.log(addNumbers(3, 6));
+// console.log('Hello world!')
+
+const category = document.querySelectorAll('.category');
