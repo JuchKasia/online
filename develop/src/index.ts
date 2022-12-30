63c52c677ae29df8522d3b2f34145b9c6d447329
Katarzyna Juchniewicz <juchkasia@gmail.com>
2022-12-31 00:26:14 +0100

    feat: make productTitle and add title to product

--- develop/src/index.ts
+++ develop/src/index.ts
@@ -32,12 +32,15 @@ console.log(arrayForCards);
 const productDetailText = document.querySelectorAll('.product-detail-text');
 const cardsStock = document.querySelectorAll('.stock');
 const priceProduct = document.querySelectorAll('.price');
+const productTitle = document.querySelectorAll('.product-title');
 
 // console.log("card " +priceProduct.length);
 // console.log("card " +cardsStock.length);
+// console.log(productTitle.length);
 
 for(let i = 0;i<productDetailText.length;i++){
     productDetailText[i].innerHTML = list[arrayForCards[i]].description;
     cardsStock[i].innerHTML = cardsStock[i].innerHTML.slice(0,4)+" "+list[arrayForCards[i]].stock;
     priceProduct[i].innerHTML = priceProduct[i].innerHTML[0] + "" + list[arrayForCards[i]].price;
+    productTitle[i].innerHTML = list[arrayForCards[i]].title;
 }
