baecc03cc815bc8d5d905f2b512ed6a0691335b4
Ihar Skavysh <igogoblin@gmail.com>
2022-12-30 16:01:00 +0300

    add one class in index html file

--- develop/src/index.ts
+++ develop/src/index.ts
@@ -1,6 +1,24 @@
 // console.log("Hello World!");
 
-import "../index";
+import "../index.js";
 import "../style.css";
 import "../style.scss";
 import "../normalize.css";
+import '../list.ts';
+
+
+// import {readFileSync} from 'fs';
+// declare function require(params:string);
+// declare let require:any;
+
+
+// const fs = require('fs');
+// import fs = require('fs');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// import * as fs from 'fs';
+import fs from 'fs';
+const list = fs.readFileSync('list.json','utf8');
+console.log(typeof (list));
+
+// const li = JSON.parse(list);
+// console.log(li[0].id)
