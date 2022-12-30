88044146ead2bcdc12588af90f3c8a39dd401281
Katarzyna Juchniewicz <juchkasia@gmail.com>
2022-12-31 00:18:35 +0100

    feat: create function buildCards, add description, psc and price

--- develop/src/index.ts
+++ develop/src/index.ts
@@ -25,5 +25,19 @@ let arrayForCards:Array<number>=[];
 arrayForCards= getRandomArray(20,99);
 console.log(arrayForCards);
 
-
 // function getRandomArray finish -----------------------------------------
+
+
+//  function buildCards -------------------------------------------------
+const productDetailText = document.querySelectorAll('.product-detail-text');
+const cardsStock = document.querySelectorAll('.stock');
+const priceProduct = document.querySelectorAll('.price');
+
+// console.log("card " +priceProduct.length);
+// console.log("card " +cardsStock.length);
+
+for(let i = 0;i<productDetailText.length;i++){
+    productDetailText[i].innerHTML = list[arrayForCards[i]].description;
+    cardsStock[i].innerHTML = cardsStock[i].innerHTML.slice(0,4)+" "+list[arrayForCards[i]].stock;
+    priceProduct[i].innerHTML = priceProduct[i].innerHTML[0] + "" + list[arrayForCards[i]].price;
+}
