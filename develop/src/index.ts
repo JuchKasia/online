b8a5521c1b01ffbea45c7cfbbb15bfd0831295c1
Katarzyna Juchniewicz <juchkasia@gmail.com>
2022-12-30 22:48:28 +0100

    feat: add function getRandomArray to undex.ts and import list.ts

--- develop/src/index.ts
+++ develop/src/index.ts
@@ -5,3 +5,25 @@ import "../style.css";
 import "../style.scss";
 import "../normalize.css";
 
+import {list} from '../list';
+
+//  function getRandomArray -------------------------------------------------
+// eslint-disable-next-line prefer-const
+let randomArray: number[] = [];
+function getRandomArray(m:number,max:number) {
+  for(let i = 0;i<m;i++){
+    const num:number = Math.floor(Math.random() * max);
+    if(!randomArray.includes(num)){
+      randomArray.push(num);
+    } else {
+      m--;
+    }
+  }
+return randomArray;
+}
+let arrayForCards:Array<number>=[];
+arrayForCards= getRandomArray(20,99);
+console.log(arrayForCards);
+
+
+// function getRandomArray finish -----------------------------------------
